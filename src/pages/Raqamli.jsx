acf274d8729bcf8f6863eeed62d1b73cdcc40a46
tempiltin <temurbekshukurov0707@gmail.com>
2024-01-23 01:00:02 +0500

    ads

--- src/pages/Raqamli.jsx
+++ src/pages/Raqamli.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import raqamli from "../MariaDB/raqamli.json";
 const Raqamli = () => {
-  return (
-    <div>Raqamli</div>
-  )
-}
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState([]);
+  const handleSearch = async (event) => {
+    const value = event.target.value;
+    await setQuery(value);
 
-export default Raqamli
+    const results = await raqamli.filter((test) =>
+      test.savol.toLowerCase().includes(value.toLowerCase())
+    );
+    await setResult(results);
+  };
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-12 col-sm-10">
+          <>
+            <label htmlFor="exampleDataList" className="form-label">
+              Savollarni qidirish
+            </label>
+            <input
+              value={query}
+              onChange={handleSearch}
+              className="form-control p-3"
+              placeholder="Savollarni qidiring......!"
+            />
+            <button
+              className="btn btn-info mt-1 wwww"
+              onClick={() => setQuery("")}
+            >
+              x
+            </button>
+          </>
+        </div>
+
+        <div className="col-12">
+          <div className="row mt-4 justify-content-center">
+            <div className="col-lg-8">
+              <div className="box p-2">
+                {result.length > 0 ? (
+                  result.map((item, index) => (
+                    <div key={index}>
+                      <h3>savol: {item.savol}</h3>
+                      <h2>Javob: {item.javob}</h2>
+                    </div>
+                  ))
+                ) : (
+                  <p>Hech qanday natija topilmadi.</p>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Raqamli;
